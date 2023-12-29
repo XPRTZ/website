d0d9fd904242ac2c9cdaa131a05364d70143aa79
Maarten Louage <me@maartenlouage.nl>
2023-12-29 15:25:40 +0100

    Add standalone config

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,7 @@ import { unifiedConditional } from 'unified-conditional'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  output: 'standalone',
 }
 
 function remarkMDXLayout(source, metaName) {
