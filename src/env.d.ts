1e624afd42bb7a16de2f7d74dd6d34c7849a4890
Matthijs Wagemakers <wagemakers.m@gmail.com>
2024-04-21 10:24:21 +0200

    Add STRAPI integration for testing end-to-end flow (#21)
    * Quick and dirty strapi integration
    
    * Remove example posts
    
    * adding build time env variables
    
    * Remove obsolete collectionDefinition
    
    * Fix pipeline
    
    * Reove obsolete content collection reference

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly STRAPI_URL: string;
+  readonly STRAPI_TOKEN: string;
+}
