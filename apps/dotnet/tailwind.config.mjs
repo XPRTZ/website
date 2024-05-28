af9a6b89728756e48e22bd0fbe950da390dd689a
Alex Schouls <alex_schouls@live.com>
2024-05-28 16:25:19 +0200

    added tailwind integration in Cloud, Dotnet and UI lib

---
+++ apps/dotnet/tailwind.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./../../libs/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
+  theme: {
+    colors: {
+      primary: {
+        50: "#f5faf3",
+        100: "#e7f4e4",
+        200: "#d1e7cb",
+        300: "#abd4a1",
+        400: "#7fb870",
+        500: "#5e9e4e",
+        600: "#487e3b",
+        700: "#3b6431",
+        800: "#32502b",
+        900: "#2a4324",
+        950: "#132310",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
+
