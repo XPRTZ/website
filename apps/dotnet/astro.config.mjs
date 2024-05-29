7c626dcf982d8bfca07d3f3628a96befde19fa71
Maarten Louage <me@maartenlouage.nl>
2024-05-29 14:10:29 +0200

    (feat) eerste opzet met layout

--- apps/dotnet/astro.config.mjs
+++ apps/dotnet/astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "https://xprtz.net",
+  integrations: [tailwind(), sitemap(), react()]
 });
---
+++ libs/ui/src/Container.tsx
@@ -0,0 +1,17 @@
+import React, { type ReactNode } from 'react';
+
+interface ContainerProps {
+  children: ReactNode;
+}
+
+export default function Container({ children }: ContainerProps) {
+  return (
+    <div className="overflow-hidden">
+      <div className="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-32">
+        <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
