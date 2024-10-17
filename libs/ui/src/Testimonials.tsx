beab08ae11c88bab45d53987eca51068135819f2
Erik-Jan Nijhof <ej.nijhof@hotmail.com>
2024-10-17 15:15:32 +0200

    Teksten verplaatst naar site-specifieke files

--- libs/ui/src/Testimonials.tsx
+++ libs/ui/src/Testimonials.tsx
@@ -1,103 +1,36 @@
-const featuredTestimonial = {
-  body: "Wij hebben samengewerkt met XPRTZ voor een migratie van onze omgevingen naar de Azure Cloud. Hun technische expertise is indrukwekkend en ze wisten ons landschap boven verwachting te verbeteren. Zeer aan te raden voor bedrijven die op zoek zijn naar een betrouwbare partner.",
-  author: {
-    name: "Brenna Goyette",
-    handle: "brennagoyette",
-    imageUrl:
-      "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=1024&h=1024&q=80",
-    logoUrl: "https://tailwindui.com/img/logos/savvycal-logo-gray-900.svg",
-  },
-};
-const testimonials = [
-  [
-    [
-      {
-        body: "Super bedankt, Jasper en Dennis! De cloud engineer die jullie stuurden voor ons Azure project was echt een topper. Hij had overal een oplossing voor en maakte het hele proces zoveel makkelijker voor ons. We hadden dit zonder jullie expertise nooit zo snel voor elkaar gekregen, echt blij met de samenwerking!",
-        author: {
-          name: "Leslie Alexander",
-          handle: "lesliealexander",
-          imageUrl:
-            "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-        },
-      },
-    ],
-    [
-      {
-        body: "XPRTZ bestaat uit een top-team van super behulpzame mensen, zij hebben ons Azure project zoveel makkelijker gemaakt!",
-        author: {
-          name: "Lindsay Walton",
-          handle: "lindsaywalton",
-          imageUrl:
-            "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-        },
-      },
-    ],
-  ],
-  [
-    [
-      {
-        body: "De cloud engineer van XPRTZ die ons hielp met de opzet van onze solution architecture was scherp, professioneel en efficiënt. Hij wist direct wat er nodig was en leverde zonder gedoe een prima resultaat. Een waardevolle kracht voor elk IT-project.",
-        author: {
-          name: "Tom Cook",
-          handle: "tomcook",
-          imageUrl:
-            "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-        },
-      },
-    ],
-    [
-      {
-        body: "Een dikke dankjewel aan Jasper en zijn team! Altijd een fijne ervaring om met zulke pros te werken. Tot de volgende!",
-        author: {
-          name: "Leonard Krasner",
-          handle: "leonardkrasner",
-          imageUrl:
-            "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-        },
-      },
-    ],
-  ],
-];
+import type { NestedTestimonials, Testimonial } from "./Types";
+
+interface ITestimonials {
+  testimonialsGreeting: string;
+  testimonialsDescription: string;
+  nestedTestimonials: NestedTestimonials;
+  featuredTestimonial: Testimonial;
+}
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function Testimonials() {
+export default function Testimonials(
+  {
+    testimonialsGreeting,
+    testimonialsDescription,
+    nestedTestimonials,
+    featuredTestimonial
+  }: ITestimonials
+) {
   return (
     <div className="relative isolate bg-white pb-32 pt-24 sm:pt-32">
-      <div
-        className="absolute inset-x-0 top-1/2 -z-10 -translate-y-1/2 transform-gpu overflow-hidden opacity-30 blur-3xl"
-        aria-hidden="true"
-      >
-        <div
-          className="ml-[max(50%,38rem)] aspect-[1313/771] w-[82.0625rem] bg-gradient-to-tr from-primary-100 to-primary-800"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-        />
+      <div className="absolute inset-x-0 top-1/2 -z-10 -translate-y-1/2 transform-gpu overflow-hidden opacity-30 blur-3xl" aria-hidden="true">
+        <div className="ml-[max(50%,38rem)] aspect-[1313/771] w-[82.0625rem] bg-gradient-to-tr from-primary-100 to-primary-800" style={{ clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }}/>
       </div>
-      <div
-        className="absolute inset-x-0 top-0 -z-10 flex transform-gpu overflow-hidden pt-32 opacity-25 blur-3xl sm:pt-40 xl:justify-end"
-        aria-hidden="true"
-      >
-        <div
-          className="ml-[-22rem] aspect-[1313/771] w-[82.0625rem] flex-none origin-top-right rotate-[30deg] bg-gradient-to-tr from-primary-100 to-primary-800 xl:ml-0 xl:mr-[calc(50%-12rem)]"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-        />
+      <div className="absolute inset-x-0 top-0 -z-10 flex transform-gpu overflow-hidden pt-32 opacity-25 blur-3xl sm:pt-40 xl:justify-end" aria-hidden="true">
+        <div className="ml-[-22rem] aspect-[1313/771] w-[82.0625rem] flex-none origin-top-right rotate-[30deg] bg-gradient-to-tr from-primary-100 to-primary-800 xl:ml-0 xl:mr-[calc(50%-12rem)]" style={{ clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }}/>
       </div>
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-xl text-center">
-          <h2 className="text-lg font-semibold leading-8 tracking-tight text-primary-600">
-            Reviews
-          </h2>
-          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Wat onze klanten over ons denken
-          </p>
+          <h2 className="text-lg font-semibold leading-8 tracking-tight text-primary-600">{testimonialsGreeting}</h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{testimonialsDescription}</p>
         </div>
         <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 grid-rows-1 gap-8 text-sm leading-6 text-gray-900 sm:mt-20 sm:grid-cols-2 xl:mx-0 xl:max-w-none xl:grid-flow-col xl:grid-cols-4">
           <figure className="rounded-2xl bg-white shadow-lg ring-1 ring-gray-900/5 sm:col-span-2 xl:col-start-2 xl:row-end-1">
@@ -107,9 +40,8 @@ export default function Testimonials() {
             <figcaption className="flex flex-wrap items-center gap-x-4 gap-y-4 border-t border-gray-900/10 px-6 py-4 sm:flex-nowrap">
               <img
                 className="h-10 w-10 flex-none rounded-full bg-gray-50"
-                src={featuredTestimonial.author.imageUrl}
-                alt=""
-              />
+                src={featuredTestimonial.author.imageURL}
+                alt=""/>
               <div className="flex-auto">
                 <div className="font-semibold">
                   {featuredTestimonial.author.name}
@@ -118,12 +50,11 @@ export default function Testimonials() {
               </div>
               <img
                 className="h-10 w-auto flex-none"
-                src={featuredTestimonial.author.logoUrl}
-                alt=""
-              />
+                src={featuredTestimonial.author.logoURL}
+                alt=""/>
             </figcaption>
           </figure>
-          {testimonials.map((columnGroup, columnGroupIdx) => (
+          {nestedTestimonials.map((columnGroup, columnGroupIdx) => (
             <div
               key={columnGroupIdx}
               className="space-y-8 xl:contents xl:space-y-0"
@@ -133,7 +64,7 @@ export default function Testimonials() {
                   key={columnIdx}
                   className={classNames(
                     (columnGroupIdx === 0 && columnIdx === 0) ||
-                      (columnGroupIdx === testimonials.length - 1 &&
+                      (columnGroupIdx === nestedTestimonials.length - 1 &&
                         columnIdx === columnGroup.length - 1)
                       ? "xl:row-span-2"
                       : "xl:row-start-1",
@@ -151,7 +82,7 @@ export default function Testimonials() {
                       <figcaption className="mt-6 flex items-center gap-x-4">
                         <img
                           className="h-10 w-10 rounded-full bg-gray-50"
-                          src={testimonial.author.imageUrl}
+                          src={testimonial.author.imageURL}
                           alt=""
                         />
                         <div>
