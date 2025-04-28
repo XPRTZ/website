0c4df90d9c5a7352c6d1eae22ee2669189416eaf
Maarten Louage <me@maartenlouage.nl>
2025-04-28 13:56:44 +0200

    (feat) add footer component

--- libs/cms/models/globalSettings.ts
+++ libs/cms/models/globalSettings.ts
@@ -1,8 +1,16 @@
 import { Page } from "../index.js";
+import { AlgemeneVoorwaarden } from "./algemeneVoorwaarden.js";
+import { Social } from "./social.js";
 
 export type GlobalSettings = {
   documentId: string;
   site: string;
   locale: string;
   pages: Page[] | null;
-}
+  algemeneVoorwaarden: AlgemeneVoorwaarden;
+  socials: Social[];
+  straat: string;
+  postcode: string;
+  gemeente: string;
+};
+
---
+++ libs/cms/models/social.ts
@@ -0,0 +1,9 @@
+import { Image } from "./image.js";
+
+export type Social = {
+  title: string;
+  link: string;
+  isEnabled: boolean;
+  icon: Image;
+};
+
