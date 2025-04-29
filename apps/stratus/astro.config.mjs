a5f19b06b010fa23838ae33964ddcef6a076282b
Maarten Louage <me@maartenlouage.nl>
2025-04-29 14:09:34 +0200

    (feat) add Stratus

---
+++ apps/stratus/astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+
+export default defineConfig({
+  site: "https://stratus.xprtz.net",
+  integrations: [tailwind(), sitemap(), react()],
+});
---
+++ apps/stratus/tailwind.config.mjs
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./../../libs/ui/src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: "#f5faf3",
+          100: "#e7f4e4",
+          200: "#d1e7cb",
+          300: "#abd4a1",
+          400: "#7fb870",
+          500: "#5e9e4e",
+          600: "#487e3b",
+          700: "#3b6431",
+          800: "#32502b",
+          900: "#2a4324",
+          950: "#132310",
+        },
+      },
+    },
+  },
+  plugins: [
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    require('@tailwindcss/typography')
+  ],
+};
