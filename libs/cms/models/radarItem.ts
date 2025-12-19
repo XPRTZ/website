11cdbc050fa4c7af641693e47826369374140d99
Dick van Hirtum <dick@xprtz.net>
2025-12-19 09:49:28 +0100

    added radar item model

---
+++ libs/cms/models/radarItem.ts
@@ -0,0 +1,17 @@
+import { ListItem } from "./listItem.js";
+import { Tag } from "./tag.js";
+
+export type RadarQuadrant = "Techniques" | "Tools" | "Platforms" | "Languages & Frameworks";
+export type RadarRing = "Adopt" | "Trial" | "Assess" | "Hold";
+
+export type RadarItem = {
+  slug: string;
+  quadrant: RadarQuadrant;
+  ring: RadarRing;
+  title: string;
+  description: string;
+  pros: ListItem[];
+  cons: ListItem[];
+  conclusion: string;
+  tags: Tag[];
+}
